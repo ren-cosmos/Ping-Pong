e56184bd0ecb1ccde06946b3297a18159fbfe237
ren-cosmos <shivamgrover50@gmail.com>
2022-02-06 00:00:40 -0330

    Updating Paddle.java

--- Paddle.java
+++ Paddle.java
@@ -1,5 +1,6 @@
 package pingPongGame;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
@@ -9,9 +10,10 @@ public class Paddle extends Rectangle{
 	int id; // store the player's id (1 or 2)
 	int yVelocity; // defines the velocity of the  vertical movement of the paddle
 
-	public Paddle()
+	public Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id)
 	{
-		
+		super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
+		this.id = id;
 	}
 	
 	public void keyPressed(KeyEvent e)
@@ -36,6 +38,10 @@ public class Paddle extends Rectangle{
 	
 	public void draw(Graphics g)
 	{
-		
+		if (id == 1)
+			g.setColor(Color.BLUE);
+		else
+			g.setColor(Color.RED);
+		g.fillRect(x, y, width, height); // x, y, width, height are the fields of Rectangele Class
 	}
 }
