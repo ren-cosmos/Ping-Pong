3590aa7461a3e57a199b599553848f2eed344644
ren-cosmos <shivamgrover50@gmail.com>
2022-02-27 23:59:40 -0330

    Updating Score Class
    Represented the scores of each players and display it along with a line in the center.

--- Score.java
+++ Score.java
@@ -1,5 +1,7 @@
 package pingPongGame;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
@@ -10,11 +12,18 @@ public class Score extends Rectangle{
 	
 	public Score(int GAME_WIDTH, int GAME_HEIGHT)
 	{
-		
+		Score.GAME_WIDTH = GAME_WIDTH;
+		Score.GAME_HEIGHT = GAME_HEIGHT;
 	}
 	
 	public void draw(Graphics g)
 	{
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("Consolas", Font.PLAIN, 60));;
 		
+		g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
+		
+		g.drawString(String.valueOf(player1/10)+String.valueOf(player1%10), GAME_WIDTH/2-85, 50);
+		g.drawString(String.valueOf(player2/10)+String.valueOf(player2%10), GAME_WIDTH/2+20, 50);
 	}
 }
